5a9d0eb57f24588560eb3283c005b525283d69a6
Abhigyan Prakash <72396135+Abhigyan2010@users.noreply.github.com>
2020-12-12 21:19:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    height:360,
+    width:360,
+    img_format:'png',
+    png_quality:100
+});
+camera=document.getElementById("live_image");
+Webcam.attach(camera);
+function Take(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("captured_image").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log("ml5 version:",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xOukSt1t1/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("model loaded!");
+}
